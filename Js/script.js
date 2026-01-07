f5e76eeb484423d0adae7461f5e9e51375af232c
rizqimaulana-design/Aplikasi-Catatan--Notes-App- <rizqiww8@gmail.com>
2026-01-07 10:26:37 +0700

    git6

--- Js/script.js
+++ Js/script.js
@@ -6,3 +6,12 @@ function loadNotes(){
     const notes = JSON.parse(localStorange.getItem("notes")) || [];
     notes.forEach(note => createNote(note));
 }
+
+//simpan semua catatan
+function saveNotes(){
+    const notes = [];
+    document.querySelectorAll(".note textarea").forEach(textarea => {
+        notes.push(textarea.value);
+    });
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
